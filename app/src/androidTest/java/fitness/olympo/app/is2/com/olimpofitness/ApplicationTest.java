0c23dece1e5087a0bee4550941dd352e98b02689
Sergio <sergio.12.educando@gmail.com>
2015-10-05 11:29:02 -0500

    Inicie Repositorio

---
+++ app/src/androidTest/java/fitness/olympo/app/is2/com/olimpofitness/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fitness.olympo.app.is2.com.olimpofitness;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
